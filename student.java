f98575dd82e2fac54ca8cdaf9e6bc021f129a68a
liangjiahong111 <1106149934@qq.com>
2021-01-15 17:13:07 +0800

    commit four

--- student.java
+++ student.java
@@ -1,6 +1,8 @@
 import java.util.*
 class student{
-  private string name;
-  private string address;
-  private string email;   //修改的内容
+      private string name;
+      private string address;
+      private string email;
+      private String telephone;   //新增电话属性
 }
+//执行以下命令将修改后的student.java提交到本地仓库blg分支中。
